ef572c9370f5effd2506d893a291705a0e13ab87
Rory <redrory@gmail.com>
2015-03-12 21:37:04 -0500

    local vs production keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :instagram, Rails.application.secrets.instagram_api_key, Rails.application.secrets.instagram_api_secret
   provider :instagram, ENV["instagram_api_key"] , ENV["instagram_api_secret"]
+  provider :instagram, ENV["instagram_api_key_local"] , ENV["instagram_api_secret_local"]
 end
